325d51d9ceb3af58a94864664483a07b048a86e6
Vincent <3d314831994@gmail.com>
2019-10-23 16:01:56 +1100

    Create Million that move forward,Turret that rotate

---
+++ Assets/Script/Canon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+
+    public LayerMask ground;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray,out hit, Mathf.Infinity))
+        {
+            Vector3 newpos = new Vector3(hit.point.x,transform.position.y,hit.point.z);
+            transform.LookAt(newpos);
+
+        }
+    }
+}
---
+++ Assets/Script/Million.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Million : MonoBehaviour
+{
+
+    public Transform turret;
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.LookAt(new Vector3(turret.position.x,transform.position.y,turret.position.z));
+        transform.Translate(0,0,moveSpeed * Time.deltaTime);
+    }
+}
